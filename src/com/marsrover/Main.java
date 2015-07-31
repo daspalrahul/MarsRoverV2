e07037324b248c722ee49a7c83a3c13287319767
Rahul Daspal <daspalrahul@gmail.com>
2015-07-31 11:32:59 +0530

    Added main so that mars rover app can get input from user and run

---
+++ src/com/marsrover/Main.java
@@ -0,0 +1,23 @@
+package com.marsrover;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String roverPosition = sc.nextLine();
+            if (roverPosition == null)
+                break;
+            String commands = sc.nextLine();
+            String[] initialPosition = roverPosition.split(" ");
+            MarsRover marsRover = new MarsRover(Integer.parseInt(initialPosition[0]), Integer.parseInt(initialPosition[1]), initialPosition[2]);
+            Parser parser = new Parser(marsRover);
+            MarsRoverApp marsRoverApp = new MarsRoverApp(marsRover, parser, commands);
+            String finalRoverPosition = marsRoverApp.startRoverAndPerformOperations();
+            System.out.print(finalRoverPosition);
+        }
+    }
+}
